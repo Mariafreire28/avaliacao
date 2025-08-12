e4627ba7901b9565a140660c39dc918aeec8c82f
Mariafreire28 <g.gracas@escolar.ifrn.edu.br>
2025-08-12 17:50:37 -0300

    fim

--- avaliacao/Assets/Player.cs
+++ avaliacao/Assets/Player.cs
@@ -1,20 +1,9 @@
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    { public float velocidade = 40
-    public float andando = 4
+ { 
+     public float velocidade = 40;
+    public float forcaDoPulo = 4f;
     
     private bool noChao = false;
     private bool andando = false;
